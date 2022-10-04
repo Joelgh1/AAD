b2d5e6fc04acd50f3d8d2b1767b9e8f6fbef5b72
Joel Garrido <joelgamar@gmail.com>
2022-10-04 20:34:21 +0200

    Ut01Ex04 Secured shared preferences

---
+++ app/src/main/java/com/joelgh/aad_2022/ut01/ex03/domain/News.kt
@@ -0,0 +1,4 @@
+package com.joelgh.aad_2022.ut01.ex03.domain
+
+data class News(val id: Int, val title: String, val subtitle: String, val summary: String) {
+}
---
+++ app/src/main/java/com/joelgh/aad_2022/ut01/ex04/SecuredSharedPreferences.kt
@@ -0,0 +1,30 @@
+package com.joelgh.aad_2022.ut01.ex04
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+import com.google.gson.Gson
+import com.joelgh.aad_2022.ut01.ex03.domain.News
+
+class SecuredSharedPreferences(val applicationContext: Context){
+
+    val gson = Gson()
+    val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
+    val mainKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+
+    val sharedPrefsFile: String = "Ut01Ex04"
+    val sharedPreferences: SharedPreferences = EncryptedSharedPreferences.create(
+        sharedPrefsFile,
+        mainKeyAlias,
+        applicationContext,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+
+    fun saveNews(news: News){
+        val editor = sharedPreferences.edit()
+        editor.putString(news.id.toString(), gson.toJson(news))
+        editor.apply()
+    }
+}
